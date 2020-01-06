578e5bedc3242f041526042cb2606a1a3f8abf28
fule <2385680866@qq.com>
2020-01-06 19:19:39 +0800

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    1905API
+                    client
                 </div>
 
                 <div class="links">
